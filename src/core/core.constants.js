8c877a4c01f405e29e4e93340d6faa449060687f
silpagui <silpagui.sa@gmail.com>
2021-08-08 16:42:29 +0200

    feat(core): new constants file

--- src/core/core.constants.js
+++ src/core/core.constants.js
@@ -1,2 +1,23 @@
 export const publicURL = process.env.PUBLIC_URL;
-export const assetsURL = `${publicURL}/assets/`
+export const assetsURL = `${publicURL}/assets/`;
+
+export const statusTask = {
+  ACTIVE: "active",
+  DONE: "done",
+};
+
+export const sections = {
+  ALL: "All",
+  ACTIVE: "Active",
+  COMPLETED: "Completed",
+};
+
+export const localStorageKeys = {
+  TASK_LIST: "taskList",
+};
+
+export const buttonsTitles = [
+  sections.ALL,
+  sections.ACTIVE,
+  sections.COMPLETED,
+];
