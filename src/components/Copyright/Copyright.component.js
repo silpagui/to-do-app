5a80053dac0f1cf6c37c36df60d0407de280011a
silpagui <silpagui.sa@gmail.com>
2021-08-08 15:51:50 +0200

    feat(Copyright): new component

---
+++ src/components/Copyright/Copyright.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Copyright.styles.scss";
+
+export function Copyright() {
+  return (
+    <p className="copyright-text">
+      created by <a href="https://silpagui.github.io/">silpagui</a> -
+      devChallenges.io
+    </p>
+  );
+}
